b1fbe486c7327a9bdcd5c0d37b1584cb8b474d13
Ansh  Agarwal <anshag2007@gmail.com>
2021-04-16 11:36:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,render_template
+from pathlib import Path
+import discord
+import requests
+
+app = Flask(
+	__name__,
+	template_folder=Path(__file__).parent)
+
+@app.route('/',methods=["GET","POST"])
+def index():
+	if request.method == "POST":
+		url = request.form["url"]
+		name = request.form["name"]
+		avatar = request.form["avatar"]
+		message = request.form["message"]
+		webhook = discord.Webhook.from_url(
+			url,
+			adapter=discord.RequestsWebhookAdapter())
+		webhook.send(
+			message,
+			username=name,
+			avatar_url=avatar)
+		
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.secret_key = "secret123"
+	app.run(
+	host='0.0.0.0',
+	port=8080)
